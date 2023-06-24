4954a7dcc8dccff852faaa9030a52c7c49d49fd2
Ibrahim Ademola Adeniran <74039043+ibademola@users.noreply.github.com>
2023-06-24 13:10:23 +0800

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -2,9 +2,9 @@
    # This script calculates simple interest given principal,
    # annual rate of interest and time period in years.
    # Do not use this in production. Sample purpose only.
-   # Author: Upkar Lidder (IBM)
+   # Author: Adeniran Ibrahim
    # Additional Authors:
-   # <your GitHub username>
+   # ibademola
    # Input:
    # p, principal amount
    # t, time period in years
